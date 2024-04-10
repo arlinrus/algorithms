2b1b86579985f373b36cc7e859787361af00ca17
arlinrus <astapenkova2002@mail.ru>
2024-04-10 12:03:55 +0300

    s

--- start/base.go
+++ start/base.go
@@ -13,29 +13,19 @@ func main() {
 	emptyArr := make([]int, len(arr))
 	copy(emptyArr, arr)
 	start := time.Now()
-	bubblesort(emptyArr)
+	Bubblesort(emptyArr)
 	duration := time.Since(start)
-	fmt.Println("O(n^2)", duration)
+	fmt.Println("Время выполнения O(n^2): ", duration)
+
 }
 
 func createArr() (arr []int) {
-	sizeArr := 90900
-	arr = make([]int, sizeArr)
+	sizeArr := 15
+	arr := make([]int, sizeArr)
+
 	for i := 0; i < sizeArr; i++ {
 		arr[i] = rand.Intn(sizeArr)
+
 	}
 	return
-
-}
-
-func bubblesort(arr []int) []int {
-	for i := 0; i < len(arr)-1; i++ {
-		for j := i + 1; j < len(arr); j++ {
-			if arr[i] > arr[j] {
-				arr[i], arr[j] = arr[j], arr[i]
-			}
-		}
-
-	}
-	return arr
 }
